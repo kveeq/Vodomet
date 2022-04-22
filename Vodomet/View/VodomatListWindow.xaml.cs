61017dede1785bd22155334a16039c8bafce5835
ильсаф гарифуллин <ilsgarifullin@yandex.ru>
2022-04-22 15:32:14 +0300

    Добавьте файлы проекта.

---
+++ Vodomet/View/VodomatListWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Vodomet.View
+{
+    /// <summary>
+    /// Логика взаимодействия для VodomatListWindow.xaml
+    /// </summary>
+    public partial class VodomatListWindow : Window
+    {
+        public VodomatListWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            VodomatSettingsPage vodomatSettingsPage = new VodomatSettingsPage();
+            vodomatSettingsPage.Show();
+        }
+    }
+}
